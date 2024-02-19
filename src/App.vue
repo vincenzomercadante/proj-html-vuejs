751cf2c360eec53f485eca83719bc77d5cbb2e3a
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-02-19 12:23:56 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+// import components
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+import AppFooter from "./components/AppFooter.vue";
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
+};
+</script>
+
+<template></template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+</style>
