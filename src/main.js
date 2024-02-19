0a07565826b1992bf7eacbed5f88c830b5144e22
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-02-19 12:51:51 +0100

    feat: add bootstrap style and js & FontAwsome's icons

--- src/main.js
+++ src/main.js
@@ -1,5 +1,55 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+// import bootstrap
+import * as bootstrap from "bootstrap";
+
+// ? import fontawesome
+
+/* import the fontawesome core */
+import { library } from "@fortawesome/fontawesome-svg-core";
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+/* import specific icons */
+import {
+  faPhoneFlip,
+  faHouseChimney,
+  faLink,
+  faChevronUp,
+} from "@fortawesome/free-solid-svg-icons";
+import {
+  faFacebookF,
+  faXTwitter,
+  faInstagram,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+import {
+  faEnvelope,
+  faCircleRight,
+  faCopyright,
+} from "@fortawesome/free-regular-svg-icons";
+
+/* add icons to the library */
+library.add(
+  faPhoneFlip,
+  faHouseChimney,
+  faLink,
+  faChevronUp,
+  faFacebookF,
+  faXTwitter,
+  faInstagram,
+  faYoutube,
+  faEnvelope,
+  faCircleRight,
+  faCopyright
+);
+
+const app = createApp(App);
+
+// add icons component in app
+app.component("font-awesome-icon", FontAwesomeIcon);
+
+app.mount("#app");
