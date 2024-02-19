fee5bdc978fd89239ac9bc8c58e1b0ca31f32629
vincenzomercadante <vincenzo.mercadante0@outlook.it>
2024-02-19 16:03:02 +0100

    feat: add site-intro & navbar

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,56 @@ import AppMain from "./components/AppMain.vue";
 import AppFooter from "./components/AppFooter.vue";
 
 export default {
+  data() {
+    return {
+      headerItems: {
+        logo: "../assets/img/site logo/avada-drivers-logo-1x.png",
+        links: [
+          {
+            linkName: "home",
+            href: "#",
+            active: true,
+            badges: [],
+          },
+          {
+            linkName: "about",
+            href: "#",
+            active: false,
+            badges: [],
+          },
+          {
+            linkName: "prices",
+            href: "#",
+            active: false,
+            badges: [],
+          },
+          {
+            linkName: "courses",
+            href: "#",
+            active: false,
+            badge: [{ text: "new", type: "new-item" }],
+          },
+          {
+            linkName: "locations",
+            href: "#",
+            active: false,
+            badges: [],
+          },
+          {
+            linkName: "blog",
+            href: "#",
+            active: false,
+            badges: [],
+          },
+        ],
+        button: {
+          text: "book now",
+          href: "#",
+        },
+      },
+    };
+  },
+
   components: {
     AppHeader,
     AppMain,
@@ -13,7 +63,16 @@ export default {
 };
 </script>
 
-<template></template>
+<template>
+  <!-- page header -->
+  <AppHeader
+    :logo="headerItems.logo"
+    :links="headerItems.links"
+    :button="headerItems.button"
+  />
+  <AppMain />
+  <AppFooter />
+</template>
 
 <style lang="scss">
 @use "./styles/general.scss" as *;
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,7 +1,99 @@
 <script>
-export default {};
+export default {
+  props: {
+    button: Object,
+    links: Array,
+    logo: String,
+  },
+
+  methods: {
+    generateUrl(path) {
+      return new URL(path, import.meta.url).href;
+    },
+  },
+};
 </script>
 
-<template></template>
+<template>
+  <header>
+    <section>
+      <div class="container">
+        <span>We have a 95% Successful Pass Rate!</span>
+        <div>
+          <font-awesome-icon icon="fa-solid fa-phone-flip" class="me-3" />
+          <span>Give us a call to book your tuition! 1-800-555-555</span>
+        </div>
+      </div>
+    </section>
+    <section>
+      <nav>
+        <div class="container">
+          <div class="row">
+            <div class="col-3">
+              <img :src="generateUrl(logo)" alt="" />
+            </div>
+            <div class="col-7">
+              <ul>
+                <li
+                  v-for="link in links"
+                  class="me-5"
+                  :class="link.active ? 'active' : ''"
+                >
+                  <a :href="link.href">{{ link.linkName }}</a>
+                </li>
+              </ul>
+            </div>
+            <div class="col-2"></div>
+          </div>
+        </div>
+      </nav>
+    </section>
+  </header>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../styles/partials/variables" as *;
+@use "../styles/partials/mixins" as *;
+
+header {
+  min-height: 750px;
+  background: transparent
+    url("../assets/img/backgrounds/homepage-hero-background.jpg");
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+
+  section:first-child {
+    padding: 10px 0;
+    background-color: $site-introduction-bg;
+
+    .container {
+      @include center-vertically;
+      justify-content: space-between;
+      color: $txt-color;
+      font-size: 0.8rem;
+    }
+  }
+  nav {
+    padding: 2rem 0;
+    background-color: rgba($color: #333, $alpha: 0.5);
+    @include center-vertically;
+
+    .col-7 {
+      @include center-vertically;
+      justify-content: center;
+      text-transform: uppercase;
+      color: $secondary-color;
+      font-weight: bold;
+
+      li {
+        padding-bottom: 5px;
+      }
+      li.active {
+        color: $primary-color;
+        border-bottom: 2px solid currentColor;
+      }
+    }
+  }
+}
+</style>
